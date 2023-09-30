2932369106d8088116c2c262e21007cd3aa00b10
Himesh S <himss@amazon.com>
2023-09-30 23:12:00 +0530

    initial commit with api app creation

---
+++ rest-api/walkmearound/src/main/java/com/app/walkmearound/WalkmearoundApplication.java
@@ -0,0 +1,13 @@
+package com.app.walkmearound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkmearoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalkmearoundApplication.class, args);
+    }
+
+}
---
+++ rest-api/walkmearound/src/test/java/com/app/walkmearound/WalkmearoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.walkmearound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkmearoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
